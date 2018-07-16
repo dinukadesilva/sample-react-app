b3800e72ec5d5ab1c3804af862f0a3f84133e8cc
dinukadesilva <l.dinukadesilva@gmail.com>
2018-07-16 11:29:15 +0530

    Adding the notes list model

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            notes: [
+                {title: "Security Assignment", description: "The deadline is on 23rd"},
+                {title: "Concert Rehearsals", description: "25th and 26th in the evening"}
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="App">
@@ -13,6 +24,14 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
+                <ul>
+                    {this.state.notes.map((note) => {
+                        return <li>
+                            <strong>{note.title}</strong>
+                            <p>{note.description}</p>
+                        </li>
+                    })}
+                </ul>
             </div>
         );
     }
